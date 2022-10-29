25b82deed7fbb0bfaf080db15de5f8006402d91b
jinhyung.noh <jinhyung.noh@voyagerx.com>
2022-10-29 22:03:12 +0900

    redirect or rewrite and use env to hide api-key

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const API_KEY = process.env.MOVIE_API_KEY;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    // 실제로 다른 url로 보내는 것
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    // redirect but! url은 변하지 않음!
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,51 @@
 import React, { useEffect, useState } from "react";
 import SEO from "../components/SEO";
 
-const API_KEY = "16127135bb3944f7906c90af1ebdd09d";
-
 const Home = () => {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      ).then((response) => response.json());
+      const { results } = await fetch(`/api/movies`).then((response) =>
+        response.json()
+      );
       setMovies(results);
     })();
-  });
+  }, []);
   return (
-    <div>
+    <div className="container">
       <SEO title={"Home"}></SEO>
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => {
         return (
-          <div key={movie.id}>
+          <div className="movie" key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            ></img>
             <h4>{movie.original_title}</h4>
           </div>
         );
       })}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
